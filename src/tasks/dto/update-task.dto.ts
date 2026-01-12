30e879479c1f70a4e0f6b77934caded6eb3bf087
cau4alves <cau4alves@gmail.com>
2026-01-12 16:49:36 -0300

    feat: added partial type

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,13 +1,22 @@
+import { PartialType } from "@nestjs/mapped-types";
 import { IsBoolean, IsOptional, IsString } from "class-validator"
+import { CreateTaskDTO } from "./create-task.dto";
 
-export class UpdateTaskDTO {
-    @IsString()
-    @IsOptional()
-    readonly name?: string
+// export class UpdateTaskDTO {
+//     @IsString()
+//     @IsOptional()
+//     readonly name?: string
 
-    @IsString()
-    @IsOptional()
-    readonly description?: string
+//     @IsString()
+//     @IsOptional()
+//     readonly description?: string
+
+//     @IsBoolean()
+//     @IsOptional()
+//     readonly completed?: boolean
+// }
+
+export class UpdateTaskDTO extends PartialType(CreateTaskDTO) {
 
     @IsBoolean()
     @IsOptional()
