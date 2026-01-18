ebc43b47b6970ba82ef373f8bc799443d9f282ec
cau4alves <cau4alves@gmail.com>
2026-01-18 13:57:14 -0300

    feat: added interceptors

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,15 +1,17 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './entities/task.entity';
 import { CreateTaskDTO } from './dto/create-task.dto';
 import { UpdateTaskDTO } from './dto/update-task.dto';
 import { PaginationDTO } from 'src/common/dto/pagination.dto';
+import { LoggerInterceptor } from 'src/common/interceptors/logger.interceptor';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private readonly taskService: TasksService) {}
 
     @Get()
+    @UseInterceptors(LoggerInterceptor)
     async findAll(@Query() paginationDTO: PaginationDTO) {
         return await this.taskService.listAllTasks(paginationDTO)
     }
