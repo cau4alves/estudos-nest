c6449309210690c09df6fc8943c99e3cc10a395c
cau4alves <cau4alves@gmail.com>
2026-02-05 01:55:23 -0300

    feat: addded create, get and update to the user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdateUserDTO } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
+    constructor(private readonly userService: UsersService) {}
 
     @Get(':id')
-    findOneUser() {
-        return 'rota funcionando'
+    async findOneUser(@Param('id', ParseIntPipe) id: number) {
+        return await this.userService.findOne(id)
+    }
+
+    @Post()
+    async createUser(@Body() createUserDTO: CreateUserDTO) {
+        return await this.userService.create(createUserDTO)
+    }
+
+    @Patch(':id')
+    async updateUser(@Param('id', ParseIntPipe) id: number, @Body() body: UpdateUserDTO) {
+        return await this.userService.update(id, body)
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,83 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { throwError } from 'rxjs';
+import { UpdateUserDTO } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private prisma: PrismaService) { }
+
+    async findOne(id: number) {
+        const user = await this.prisma.user.findFirst({
+            where: {
+                id: id
+            },
+            select: {
+                id: true,
+                email: true,
+                name: true
+            }
+        })
+
+        if (user) return user
+
+        throw new NotFoundException('Usuário não encontrado')
+    }
+
+    async create(body: CreateUserDTO) {
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    name: body.name,
+                    email: body.email,
+                    passwordHash: body.password
+                },
+                select: {
+                    id: true,
+                    name: true,
+                    email: true
+                }
+            })
+
+            return user
+        } catch (error) {
+            console.log(error)
+            throw new BadRequestException('Falha ao cadastrar usuário')
+        }
+    }
+
+    async update(id: number, body: UpdateUserDTO) {
+        try {
+            const user = await this.prisma.user.findFirst({
+                where: {
+                    id: id
+                }
+            })
+
+            if (!user) {
+                throw new NotFoundException('Usuário não encontrado')
+            }
+
+            const updatedUser = await this.prisma.user.update({
+                where: {
+                    id: user.id
+                }, 
+                data: {
+                    name: body.name ? body.name : user.name,
+                    passwordHash: body.password ? body.password : user.passwordHash
+                },
+                select: {
+                    id: true,
+                    name: true,
+                    email: true
+                }
+            })
+
+            return updatedUser
+        } catch (error) {
+            console.log(error)
+            throw new BadRequestException('Falha ao atualizar o usuário')
+        }
+    }
+}
